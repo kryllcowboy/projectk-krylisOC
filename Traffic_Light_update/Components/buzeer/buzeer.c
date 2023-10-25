4ec0a97bf0c897cb2dc44ef055beb506f1b3a1ed
kryllcowboy <kirill77577@gmail.com>
2023-10-25 14:33:25 +0300

    Traffic_Light_update

--- Traffic_Light_update/Components/buzeer/buzeer.c
+++ Traffic_Light_update/Components/buzeer/buzeer.c
@@ -1,6 +1,12 @@
 #include "buzzer.h"
 #include "main.h"
 #include "stm32f4xx_it.h"
+#include "stm32f4xx_hal.h"
+
+
+uint16_t frequency = 1000;
+uint16_t frequency_step = 100; 
+
 
 void buzzer_init(void)
 {
@@ -9,13 +15,19 @@ void buzzer_init(void)
 
  void buzzer_run()
 {
-//buzzer_run(TBUZ_100, TICK_2);
-  HAL_Delay(2000);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
   
-  while (1)
-  {
-//	  buzzer_run(TBUZ_50, TICK_4);
-	  HAL_Delay(2000);
-  }
+ while (1) 
+{
+    for (int i = 0; i < 10; i++) 
+   {
+      frequency += frequency_step;
+      HAL_Delay(1000 / frequency);
+     
+      if (frequency > 4000) 
+      {
+       HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+      }
+   }
 }
-  
+}  
--- buzzer/Components/buzeer/buzeer.c
+++ buzzer/Components/buzeer/buzeer.c
@@ -1,6 +1,9 @@
 #include "buzzer.h"
 #include "main.h"
 #include "stm32f4xx_it.h"
+ 
+uint16_t frequency = 1000; //starting frequency
+uint16_t frequency_step = 100; //step
 
 void buzzer_init(void)
 {
@@ -9,13 +12,21 @@ void buzzer_init(void)
 
  void buzzer_run()
 {
-buzzer_run(TBUZ_100, TICK_2);
-  HAL_Delay(2000);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+  HAL_Delay(20000); 
   
-  while (1)
-  {
-	  buzzer_run(TBUZ_50, TICK_4);
-	  HAL_Delay(2000);
-  }
-}
-  
+   while (1) 
+     {
+    for (int i = 0; i < 10; i++) 
+     {
+      frequency += frequency_step;
+      HAL_Delay(500 / frequency); 
+      HAL_Delay(1000 / frequency);
+
+      if (frequency > 4000) 
+      {
+       HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+      }
+    }
+    }
+ }
